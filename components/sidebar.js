f4cdf7171605a304d3010e34e110265c3ba2f316
kapatiuska <65649227+jordicapedo@users.noreply.github.com>
2022-05-27 18:13:05 +0200

    login, logout, next-auth

--- components/sidebar.js
+++ components/sidebar.js
@@ -2,15 +2,28 @@ import {
   HomeIcon,
   SearchIcon,
   PlusSmIcon,
-  CollectionIcon
+  CollectionIcon,
+  LogoutIcon
 } from '@heroicons/react/outline'
 import Image from 'next/image'
+import { signOut, useSession } from 'next-auth/react'
 import { HeartIcon, BookmarkIcon } from '@heroicons/react/solid'
 
 function Sidebar() {
+  const { data: session, status } = useSession()
+
+  console.log(session)
+
   return (
     <div className="border-r border-gray-900 p-5 text-sm text-gray-500">
       <div className="space-y-4">
+        <button
+          className="flex items-center space-x-2 hover:text-white"
+          onClick={() => signOut()}
+        >
+          <LogoutIcon className="h-5 w-5" />
+          <p className="font-bold">Logout</p>
+        </button>
         <button className="flex items-center space-x-2 hover:text-white">
           <HomeIcon className="h-5 w-5" />
           <p className="font-bold">Home</p>
--- pages/login.js
+++ pages/login.js
@@ -1,9 +1,32 @@
-function Login() {
+import { getProviders, signIn } from 'next-auth/react'
+import Image from 'next/image'
+
+function Login(providers) {
+  const SpotifyLogoSrc = '/images/spotify-logo-green.png'
   return (
-    <div>
-      <h1>Login page</h1>
+    <div className="flex min-h-screen w-full flex-col items-center justify-center bg-black">
+      <Image src={SpotifyLogoSrc} width={202} height={68} alt="Logo" />
+      {Object.values(providers).map(provider => (
+        <div key={provider.spotify.name}>
+          <button
+            className="mt-10 rounded-full bg-[#2dd760] py-4 px-24 text-sm font-bold tracking-[2px]"
+            onClick={() => signIn(provider.spotify.id, { callbackUrl: '/' })}
+          >
+            START SESSION
+          </button>
+        </div>
+      ))}
     </div>
   )
 }
 
 export default Login
+
+export async function getServerSideProps() {
+  const providers = await getProviders()
+  return {
+    props: {
+      providers
+    }
+  }
+}
